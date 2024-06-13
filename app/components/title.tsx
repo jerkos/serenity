50ac78e6666743811213f3381868e371b68abfd8
Marc Dubois <marc@synapse-medicine.com>
2024-06-13 21:21:05 +0200

    fix: disable scale and letter spacing on small device

--- app/components/title.tsx
+++ app/components/title.tsx
@@ -5,12 +5,12 @@ import { useEffect, useMemo, useState } from "react";
 import { useWindowSize } from "@uidotdev/usehooks";
 
 
-const TITLES = ['Complexity. With Serenity.',
-  'Pragmatic. With Elegance.'
+const TITLES = [['Complexity.', 'With Serenity.'],
+['Pragmatic.', 'With Elegance.']
 ];
 
 interface AnimatedTextProps {
-  text: string;
+  text: string[];
 }
 
 const WIDTH = 1030;
@@ -25,8 +25,8 @@ const AnimatedText = ({ text }: AnimatedTextProps) => {
     show: {
       skewX: ["0", "0", "0", "0", `${Math.random() * 32}deg`, `${Math.random() * 12}deg`, `${Math.random() * 42}deg`, `${Math.random() * 21}deg`, "0"],
       opacity: [1, 1, 1, 1, 0, 1, 0, 1, 1],
-      scale: [1, 1, 2, 2, 2, 2, 2, 2, 2.12],
-      letterSpacing: ['0', '0', '1.12rem', '1.12rem', '1.12rem', '1.12rem', '1.12rem', '1.12rem', '1.12rem'],
+      scale: [1, 1, 1.32, 1.32, 1.32, 1.32, 1.32, 1.32, 1.32],
+      letterSpacing: ['0', '0', '0.34rem', '0.34rem', '0.34rem', '0.34rem', '0.34rem', '0.34rem', '0.34rem'],
       transition: { times: [0.1, 0.5, 0.505, 0.8, 0.805, 0.81, 0.84, 0.845, 0.86], duration: 4.8, ease: "circInOut" },
     }
   }), []);
@@ -48,22 +48,17 @@ const AnimatedText = ({ text }: AnimatedTextProps) => {
 
   return (
     <AnimatePresence mode="popLayout">
-      {
-        Array.from(text).map((char, index) => {
-          return (
-            <motion.span
-              key={Math.random()}
-              variants={variants}
-              initial='hidden'
-              animate={"show"}
-              exit={{ opacity: 0, scale: 1, letterSpacing: '0', y: '1rem' }}
-            >
-              {char === ' ' ? '\u00A0' : char}
-            </motion.span>
-
-          );
-        })
-      }
+      <motion.div
+        className="flex flex-col justify-center items-center"
+        key={Math.random()}
+        variants={variants}
+        initial='hidden'
+        animate={"show"}
+        exit={{ opacity: 0, scale: 1, letterSpacing: '0', y: '1rem' }}
+      >
+        <motion.span>{text[0]}</motion.span>
+        <motion.span>{text[1]}</motion.span>
+      </motion.div>
     </AnimatePresence >
   )
 }
@@ -81,11 +76,9 @@ export const Title = () => {
   });
 
   const title = TITLES[currentTitleIdx];
-  const { width } = useWindowSize();
-  const size = width && width < WIDTH ? 'text-2xl' : 'text-4xl';
   return (
-    <section className="my-4 ">
-      <h1 className={`${size} font-semibold justify-center flex items-center`} style={{ height: '280px' }}>
+    <section className="my-8">
+      <h1 className={`text-4xl font-semibold justify-center flex items-center`} style={{ height: '260px' }}>
         <AnimatedText text={title} />
       </h1>
     </section>
